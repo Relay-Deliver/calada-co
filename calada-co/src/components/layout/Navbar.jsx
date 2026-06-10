8ed283aec8e0cab9b7c65b06cdf1107ce75d45c8
samuel-owiyo <fastechsolutions26e@gmail.com>
2026-06-10 02:00:37 -0400

    Shift logo left on mobile to avoid wishlist overlap

--- calada-co/src/components/layout/Navbar.jsx
+++ calada-co/src/components/layout/Navbar.jsx
@@ -158,7 +158,7 @@ export default function Navbar() {
           </button>
           <Link
             to="/"
-            className="absolute left-1/2 top-1/2 flex h-12 w-32 -translate-x-1/2 -translate-y-1/2 items-center justify-center sm:h-14 sm:w-44"
+            className="absolute left-[40%] top-1/2 flex h-10 w-28 -translate-x-1/2 -translate-y-1/2 items-center justify-start sm:left-1/2 sm:h-14 sm:w-44"
             onClick={() => setMobileOpen(false)}
           >
             <LogoMark className="h-full w-full" />
